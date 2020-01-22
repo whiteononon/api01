85017c24d4dbcc5fc1bf26c7b05d8d619a721c76
whiteononon <whiteon0618@gmail.com>
2020-01-22 22:26:27 +0900

    seed is finished

--- vendor/fzaninotto/faker/src/Faker/Factory.php
+++ vendor/fzaninotto/faker/src/Faker/Factory.php
@@ -4,7 +4,7 @@ namespace Faker;
 
 class Factory
 {
-    const DEFAULT_LOCALE = 'en_US';
+    const DEFAULT_LOCALE = 'ja_JP';
 
     protected static $defaultProviders = array('Address', 'Barcode', 'Biased', 'Color', 'Company', 'DateTime', 'File', 'HtmlLorem', 'Image', 'Internet', 'Lorem', 'Miscellaneous', 'Payment', 'Person', 'PhoneNumber', 'Text', 'UserAgent', 'Uuid');
 
