dad24c79154fa423e88bbd9ea57f62e6ee883a85
whiteononon <whiteon0618@gmail.com>
2020-01-22 22:53:14 +0900

    relationship created

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->word,
         'detail' => $faker->realText,
         'price' => $faker->numberBetween(100,1000),
         'stock' => $faker->randomDigit,
