24251cb703d9b3a416595a03a20764b2595b4b14
whiteononon <whiteon0618@gmail.com>
2020-01-23 00:37:35 +0900

    modified product resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,21 +6,26 @@ use Illuminate\Http\Resources\Json\Resource;
 
 class ProductResource extends Resource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-          'name' => $this->name,
-          'description' => $this->detail,
-          'price' => $this->price,
-          'stock' => $this->stock,
-          'discount' => $this->discount,
-    
-        ];
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array
+   */
+  public function toArray($request)
+  {
+    return [
+      'name' => $this->name,
+      'description' => $this->detail,
+      'price' => $this->price,
+      'stock' => $this->stock == 0 ? 'out of stock' : $this->stock,
+      'discount' => $this->discount,
+      'totalPrice' => round($this->price * (1 - ($this->discount / 100)), 2),
+      'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'no rating yet',
+      'herf' => [
+        'reviews' => route('reviews.index', $this->id)
+      ]
+
+    ];
+  }
 }
