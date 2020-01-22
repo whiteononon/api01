c6e2fba1636a6d21872b89c0a3cff6cf4447b4db
whiteononon <whiteon0618@gmail.com>
2020-01-23 00:52:19 +0900

    Product resouce created

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
   /**
    * Transform the resource collection into an array.
@@ -14,5 +15,14 @@ class ProductCollection extends ResourceCollection
    */
   public function toArray($request)
   {
+    return [
+      'name' => $this->name,
+      'totalPrice' => round($this->price * (1 - ($this->discount / 100)), 2),
+      'discount' => $this->discount,
+      'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'no rating yet',
+      'herf' => [
+        'link' => route('products.show', $this->id)
+      ]
+    ];
   }
 }
